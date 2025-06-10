12f3b4c53dd00057d769c14f5c435a3c1289f1d7
FairuzFairuz <ahmadfairuz2312@gmail.com>
2025-06-11 02:32:37 +0800

    Update LandingPage.jsx

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  Container,
-  Grid,
-  Card,
-  Button,
-  Typography,
-  CircularProgress,
-} from "@mui/material";
 
 const LandingPage = ({ user, handleLogout }) => {
   const navigate = useNavigate();
